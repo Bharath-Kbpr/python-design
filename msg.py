9f0b8051ab4ca7ff5bdfbf9fb859d04c92d90b89
Bharath-Kbpr <iamkbpr@gmail.com>
2020-07-15 23:49:52 +0530

    sms

---
+++ msg.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def send_sms(number,message):
+    url ='https://www.fast2sms.com/dev/bulk'
+    params={
+        'authorization':'Uwj9BRgtOQfNVxXCec0apMym2T31n8AJhGkdZIF7Ks5LSib6l4Ej1Hh3gwDbrUcBioTuAXMOqd5WRNxS',
+        'sender_id':'FSTSMS',
+        'message':message,
+        'language':'english',
+        'route':'p',
+        'numbers':number
+    }
+
+    response=requests.get(url,params=params)
+    dic=response.json()
+    print(dic)
+
+
+send_sms("9182780240","this is bharath")
+
